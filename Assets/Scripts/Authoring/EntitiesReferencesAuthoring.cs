fbf49476bf9f5d1256f6f7169fbb46d139ddd1b9
Hobo4rmSpace <154842024+DamianSaelee@users.noreply.github.com>
2024-07-29 15:11:17 -0700

    Initial Upload

---
+++ Assets/Scripts/Authoring/EntitiesReferencesAuthoring.cs
@@ -0,0 +1,21 @@
+using Unity.Entities;
+using UnityEngine;
+
+public class EntitiesReferencesAuthoring : MonoBehaviour {
+  public GameObject bulletPrefabGameObject;
+  public GameObject zombiePrefabEntity;
+  public class Baker : Baker<EntitiesReferencesAuthoring> {
+    public override void Bake(EntitiesReferencesAuthoring authoring) {
+      Entity entity = GetEntity(TransformUsageFlags.Dynamic);
+      AddComponent(entity, new EntitiesReferences {
+        bulletPrefabEntity = GetEntity(authoring.bulletPrefabGameObject, TransformUsageFlags.Dynamic),
+        zombiePrefabEntity = GetEntity(authoring.zombiePrefabEntity, TransformUsageFlags.Dynamic),
+      });
+    }
+  }
+}
+
+public struct EntitiesReferences : IComponentData {
+  public Entity bulletPrefabEntity;
+  public Entity zombiePrefabEntity;
+}
---
+++ Assets/Scripts/Systems/ShootAttackSystem.cs
@@ -0,0 +1,63 @@
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
+
+partial struct ShootAttackSystem : ISystem {
+  [BurstCompile]
+  public void OnUpdate(ref SystemState state) {
+    EntitiesReferences entitiesReferences = SystemAPI.GetSingleton<EntitiesReferences>();
+    foreach ((
+      RefRW<LocalTransform> localTransform,
+      RefRW<ShootAttack> shootAttack,
+      RefRO<Target> target,
+      RefRW<UnitMover> unitMover)
+      in SystemAPI.Query<
+        RefRW<LocalTransform>,
+        RefRW<ShootAttack>,
+        RefRO<Target>,
+        RefRW<UnitMover>>()) {
+
+      if (target.ValueRO.targetEntity == Entity.Null) {
+        continue;
+      }
+
+      LocalTransform targetLocalTransform = SystemAPI.GetComponent<LocalTransform>(target.ValueRO.targetEntity);
+
+      if (math.distance(localTransform.ValueRO.Position, targetLocalTransform.Position) > shootAttack.ValueRO.attackDistance) {
+        // Target too far, move closer
+        unitMover.ValueRW.targetPosition = targetLocalTransform.Position;
+        continue;
+      } else {
+        // Close enough, stop moving and attack
+        unitMover.ValueRW.targetPosition = localTransform.ValueRO.Position;
+      }
+
+      float3 aimDirection = targetLocalTransform.Position - localTransform.ValueRO.Position;
+      aimDirection = math.normalize(aimDirection);
+
+      quaternion targetRotation = quaternion.LookRotation(aimDirection, math.up());
+      localTransform.ValueRW.Rotation = math.slerp(localTransform.ValueRO.Rotation, targetRotation, SystemAPI.Time.DeltaTime * unitMover.ValueRO.rotationSpeed);
+
+      shootAttack.ValueRW.timer -= SystemAPI.Time.DeltaTime;
+      if (shootAttack.ValueRO.timer > 0f) {
+        continue;
+      }
+      shootAttack.ValueRW.timer = shootAttack.ValueRO.timerMax;
+      
+      Entity bulletEntity = state.EntityManager.Instantiate(entitiesReferences.bulletPrefabEntity);
+      float3 bulletSpawnWorldPosition = localTransform.ValueRO.TransformPoint(shootAttack.ValueRO.bulletSpawnLocalPosition);
+      SystemAPI.SetComponent(bulletEntity, LocalTransform.FromPosition(bulletSpawnWorldPosition));
+
+      RefRW<Bullet> bulletBullet = SystemAPI.GetComponentRW<Bullet>(bulletEntity);
+      bulletBullet.ValueRW.damageAmount = shootAttack.ValueRO.damageAmount;
+      bulletBullet.ValueRW.speed = shootAttack.ValueRO.speed;
+
+      RefRW<Target> bulletTarget = SystemAPI.GetComponentRW<Target>(bulletEntity);
+      bulletTarget.ValueRW.targetEntity = target.ValueRO.targetEntity;
+    }
+  }
+
+}
